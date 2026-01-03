bc96e479864bf8713e7f91cecd211a79c2e64303
vbaspire <vbaconnected@gmail.com>
2026-01-03 01:42:21 -0600

    Update App.js

--- src/App.js
+++ src/App.js
@@ -568,7 +568,7 @@ const VBAKudos = () => {
                 onChange={(e) => setCoreValue(e.target.value)}
                 className="w-full px-4 py-2 border border-gray-300 rounded-lg focus:ring-2 focus:ring-blue-500"
               >
-                <option value="">Select a core value...</option>
+                <option value="">Select a core value</option>
                 {CORE_VALUES.map((value) => (
                   <option key={value} value={value}>
                     {value}
